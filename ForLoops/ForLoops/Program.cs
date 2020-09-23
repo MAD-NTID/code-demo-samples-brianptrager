aeece24eaffdc4628bd4bc1cdc0be9a3167645ac
Brian Trager <bptnbs@rit.edu>
2020-09-23 09:37:40 -0400

    For Loops Code Demo

---
+++ ForLoops/ForLoops/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ForLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //i++: i = i + 1
+            /*
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for(int i = 20; i >= 0; i--)
+            {
+                Console.WriteLine(i);
+            }*/
+
+            //i+=5: i = i + 5;
+            /*
+            for(int i = 0; i <= 100; i+=5)
+            {
+                Console.WriteLine(i);
+            }
+
+            for(int i=1; i <= 1000; i+=i)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                if((i % 25) == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                }
+
+                Console.WriteLine(i);
+                Console.ResetColor();
+            }*/
+
+            Console.Write("Enter a number to begin: ");
+            int num = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i <= num; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+
+        }
+    }
+}
---
+++ ForLoops/LoopsWithArrays/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace LoopsWithArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] classList = { "Tyler Ackerina", "Emily Oesterling", "Conor Doherty", "Izzy Pedrizco-Carranza", "James Anderson", "Trey Lukitsch" };
+
+            Console.WriteLine($"There are {classList.Length} students in the class.");
+
+            for(int i=0; i < classList.Length; i++)
+            {
+                Console.WriteLine(classList[i]);
+            }
+
+            Console.Write("Enter the number of new students in your class?: ");
+            int newAddition = int.Parse(Console.ReadLine());
+            int oldArraySize = classList.Length;
+
+            Array.Resize(ref classList, classList.Length + newAddition);
+
+            //For loop to add new students to resized array
+            for (int i=0; i < newAddition; i++)
+            {
+                Console.Write($"{i + 1}. Student's Name: ");
+                classList[oldArraySize + i] = Console.ReadLine();
+            }
+
+            //newAddition = newAddition + classList.Length
+            newAddition += classList.Length;
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            
+            Console.WriteLine($"There are {classList.Length} students in the class.");
+            for (int i = 0; i < classList.Length; i++)
+            {
+                Console.WriteLine(classList[i]);
+            }
+        }
+    }
+}
