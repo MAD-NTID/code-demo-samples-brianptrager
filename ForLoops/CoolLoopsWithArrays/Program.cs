18432819e26e24aebff1643e8e86f4cdc8db3d9a
Brian Trager <bptnbs@rit.edu>
2020-09-25 09:22:35 -0400

    Lucky Numbers

---
+++ ForLoops/CoolLoopsWithArrays/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace CoolLoopsWithArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] classList = { "Tyler Ackerina", "Emily Oesterling", "Conor Doherty", "Izzy Pedrizco-Carranza", "James Anderson", "Trey Lukitsch" };
+
+            Console.Write("How many students are in your class?: ");
+            int classSize = int.Parse(Console.ReadLine());
+
+            string[] classList = new string[classSize];
+
+            //int[] numArray = new int[10]; //creates an array of 10 elements that only stores integers
+
+            for(int i = 0; i < classList.Length; i++)
+            {
+                Console.Write($"Enter student {i + 1}: ");
+                classList[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine($"There are {classList.Length} students in the class.");
+
+            for (int i = 0; i < classList.Length; i++)
+            {
+                Console.WriteLine(classList[i]);
+            }
+
+           /* Console.Write("Enter the number of new students in your class?: ");
+            int newAddition = int.Parse(Console.ReadLine());
+            int oldArraySize = classList.Length;
+
+            Array.Resize(ref classList, classList.Length + newAddition);
+
+            //For loop to add new students to resized array
+            for (int i = 0; i < newAddition; i++)
+            {
+                Console.Write($"{i + 1}. Student's Name: ");
+                classList[oldArraySize + i] = Console.ReadLine();
+            }
+
+            //newAddition = newAddition + classList.Length
+            newAddition += classList.Length;
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine($"There are {classList.Length} students in the class.");
+            for (int i = 0; i < classList.Length; i++)
+            {
+                Console.WriteLine(classList[i]);
+            }*/
+        }//end Main
+    }
+}
---
+++ ForLoops/LuckyNumberArray/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace LuckyNumberArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int MAX_GUESS_ATTEMPTS = 10;
+            const int MAX_NUMBER = 100;
+
+            int[] luckyNumbers = new int[3];
+            Random random = new Random();
+
+            for (int i = 0; i < luckyNumbers.Length; i++)
+            {
+                luckyNumbers[i] = random.Next(MAX_NUMBER);
+            }
+            
+            int currentGuess = 0;
+
+            Console.WriteLine($"You get {MAX_GUESS_ATTEMPTS} tries to guess a number between 1 and {MAX_NUMBER}.\n");
+
+            for (int i = 1; i <= MAX_GUESS_ATTEMPTS; i++)
+            {
+                Console.Write($"Lucky guess #{i}: ");
+                currentGuess = int.Parse(Console.ReadLine());
+
+                for (int j = 0; j < luckyNumbers.Length; j++)
+                {
+                    if (currentGuess == luckyNumbers[j])
+                    {
+                        int grandNumber = 0;
+
+                        for(int k = 0; k < luckyNumbers.Length; k++)
+                        {
+                            grandNumber += luckyNumbers[k];
+                        }
+                        Console.WriteLine("You are SO LUCKY!!!  Nicely done and congrats!");
+                        Console.WriteLine("Make a lucky guess for $10 MILLION DOLLARS!!!: ");
+                        if(int.Parse(Console.ReadLine()) == grandNumber)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.WriteLine("HOLY COW!!  You WON $10 MILLION DOLLARS!!");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Sorry, but the grand lucky number is {grandNumber}.");
+                        }
+
+                        Console.ReadLine();
+                        Environment.Exit(0);
+                    }
+                    else if (currentGuess == -1)
+                    {
+                        foreach (int lucky in luckyNumbers)
+                        {
+                            Console.Write($"{lucky} ");
+                        }
+                    }
+                }
+            }
+
+            Console.Write($"Not your lucky day!  The lucky number are ");
+            foreach(int lucky in luckyNumbers)
+            {
+                Console.Write($"{lucky} ");
+            }
+
+            //int luckyNumber = random.Next(MAX_NUMBER);
+
+            /*for(int i = 1; i <= MAX_GUESS_ATTEMPTS; i++)
+            {
+                Console.Write($"Lucky guess #{i}: ");
+                currentGuess = int.Parse(Console.ReadLine());
+
+                if (currentGuess == luckyNumber)
+                {
+                    Console.WriteLine("You are SO LUCKY!!!  Nicely done and congrats!");
+                    Console.ReadLine();
+                    Environment.Exit(0);
+                }
+                else if(currentGuess > luckyNumber)
+                {
+                    Console.WriteLine("Too high, guess lower.");
+                }
+                else
+                {
+                    Console.WriteLine("Too low, guess higher.");
+                }
+            }
+
+            Console.WriteLine($"Not your lucky day!  The lucky number is {luckyNumber}");*/
+        }
+    }
+}
