de501f7672d58ae33e56fa1aa3d294495bfa46d6
Brian Trager <bptnbs@rit.edu>
2020-10-09 10:40:45 -0400

    Added Constructors and ToString

--- OOP/CarFactory/Car.cs
+++ OOP/CarFactory/Car.cs
@@ -14,16 +14,45 @@ namespace CarFactory
         public string Model { get; set; }
         public bool IsRunning { get; set; }
 
-        public void Drive()
+        public void Drive(int miles)
         {
             if(IsRunning)
             {
-                Console.WriteLine("Car is now in drive mode.");
+                Console.WriteLine($"Car is now in drive mode. You can drive for approximately {miles} miles.");
             }
             else
             {
                 Console.WriteLine("Cannot shift into drive mode, car is not running");
             }
+        }
+
+        public Car()
+        {
+            Year = 1900;
+            Make = "Unknown";
+            Model = "Unknown";
+            GasTankCapacity = 0;
+            CarType = "N/A";
+            Color = "Black";
+            IsRunning = false;
+        }
+
+        public Car(int year, string color, int gasTankCapacity, string make, string model, string carType)
+        {
+            Year = year;
+            Color = color;
+            GasTankCapacity = gasTankCapacity;
+            Make = make;
+            Model = model;
+            CarType = carType;
+            IsRunning = true;
+        }
+
+        public override string ToString()
+        {
+            return String.Format($"Year: {Year}\nMake: {Make}\nModel: {Model}\n" +
+                $"Car Type: {CarType}\nColor: {Color}\n" +
+                $"Gas Tank Capacity: {GasTankCapacity}");
         }
     }
 }
--- OOP/CarFactory/Program.cs
+++ OOP/CarFactory/Program.cs
@@ -8,7 +8,7 @@ namespace CarFactory
         {
             //I want to create a car object
             Car myCar = new Car();
-
+            
             myCar.Color = "Red";
             myCar.CarType = "SUV";
             myCar.Make = "Ford";
@@ -16,14 +16,15 @@ namespace CarFactory
             myCar.GasTankCapacity = 18;
             myCar.Year = 2018;
             myCar.IsRunning = true;
-            myCar.Drive();
+            myCar.Drive(250);
             Console.WriteLine("Data for the first car object");
-            Console.WriteLine($"Color: {myCar.Color}");
-            Console.WriteLine($"Car Type: {myCar.CarType}");
-            Console.WriteLine($"Make: {myCar.Make}");
-            Console.WriteLine($"Model: {myCar.Model}");
-            Console.WriteLine($"Gas Tank Capacity: {myCar.GasTankCapacity}");
-            Console.WriteLine($"Year: {myCar.Year}");
+            Console.WriteLine(myCar);
+            //Console.WriteLine($"Color: {myCar.Color}");
+            //Console.WriteLine($"Car Type: {myCar.CarType}");
+            //Console.WriteLine($"Make: {myCar.Make}");
+            //Console.WriteLine($"Model: {myCar.Model}");
+            //Console.WriteLine($"Gas Tank Capacity: {myCar.GasTankCapacity}");
+            //Console.WriteLine($"Year: {myCar.Year}");
 
 
             Car jamesCar = new Car();
@@ -34,31 +35,33 @@ namespace CarFactory
             jamesCar.GasTankCapacity = 15;
             jamesCar.Year = 2012;
             jamesCar.IsRunning = false;
-            jamesCar.Drive();
+            jamesCar.Drive(350);
             Console.WriteLine("Data for the second car object");
-            Console.WriteLine($"Color: {jamesCar.Color}");
-            Console.WriteLine($"Car Type: {jamesCar.CarType}");
-            Console.WriteLine($"Make: {jamesCar.Make}");
-            Console.WriteLine($"Model: {jamesCar.Model}");
-            Console.WriteLine($"Gas Tank Capacity: {jamesCar.GasTankCapacity}");
-            Console.WriteLine($"Year: {jamesCar.Year}");
+            Console.WriteLine(jamesCar);
+            //Console.WriteLine($"Color: {jamesCar.Color}");
+            //Console.WriteLine($"Car Type: {jamesCar.CarType}");
+            //Console.WriteLine($"Make: {jamesCar.Make}");
+            //Console.WriteLine($"Model: {jamesCar.Model}");
+            //Console.WriteLine($"Gas Tank Capacity: {jamesCar.GasTankCapacity}");
+            //Console.WriteLine($"Year: {jamesCar.Year}");
 
-            Car conorCar = new Car();
-            conorCar.Color = "Grey";
-            conorCar.CarType = "Sedan";
-            conorCar.Make = "Ford";
-            conorCar.Model = "Focus";
-            conorCar.GasTankCapacity = 12;
-            conorCar.Year = 2014;
-            conorCar.IsRunning = false;
-            conorCar.Drive();
-            Console.WriteLine("Data for the first car object");
-            Console.WriteLine($"Color: {conorCar.Color}");
-            Console.WriteLine($"Car Type: {conorCar.CarType}");
-            Console.WriteLine($"Make: {conorCar.Make}");
-            Console.WriteLine($"Model: {conorCar.Model}");
-            Console.WriteLine($"Gas Tank Capacity: {conorCar.GasTankCapacity}");
-            Console.WriteLine($"Year: {conorCar.Year}");
+            Car conorCar = new Car(2014, "Grey", 12, "Ford", "Focus", "Sedan");
+            //conorCar.Color = "Grey";
+            //conorCar.CarType = "Sedan";
+            //conorCar.Make = "Ford";
+            //conorCar.Model = "Focus";
+            //conorCar.GasTankCapacity = 12;
+            //conorCar.Year = 2014;
+            //conorCar.IsRunning = false;
+            conorCar.Drive(300);
+            Console.WriteLine("Data for the third car object");
+            Console.WriteLine(conorCar);
+            //Console.WriteLine($"Color: {conorCar.Color}");
+            //Console.WriteLine($"Car Type: {conorCar.CarType}");
+            //Console.WriteLine($"Make: {conorCar.Make}");
+            //Console.WriteLine($"Model: {conorCar.Model}");
+            //Console.WriteLine($"Gas Tank Capacity: {conorCar.GasTankCapacity}");
+            //Console.WriteLine($"Year: {conorCar.Year}");
 
 
         }
