e7780346dcea6177bf213ba45c8f5963076c4f8b
Brian Trager <bptnbs@rit.edu>
2020-10-14 09:23:26 -0400

    Date and Time
    This should help with Project #2.

---
+++ DateAndTime/DateAndTime/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DateAndTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = DateTime.Now;
+
+            Console.WriteLine(dateTime.DayOfWeek);
+            Console.WriteLine(dateTime.Day);
+            Console.WriteLine(dateTime.Month);
+            Console.WriteLine(dateTime.ToLongDateString());
+            Console.WriteLine(dateTime.ToShortDateString());
+            Console.WriteLine(dateTime.ToLongTimeString());
+            Console.WriteLine(dateTime.ToShortTimeString());
+
+            if(dateTime.DayOfWeek.ToString() == "Wednesday")
+            {
+                Console.WriteLine("This is a very special day!");
+            }
+        }
+    }
+}
