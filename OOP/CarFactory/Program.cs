f33e0e2adc00eb1ee30be0d8c387b851ce51ac95
Brian Trager <bptnbs@rit.edu>
2020-10-16 09:49:17 -0400

    Crazy code by Professor BT

--- OOP/CarFactory/Program.cs
+++ OOP/CarFactory/Program.cs
@@ -6,8 +6,77 @@ namespace CarFactory
     {
         static void Main(string[] args)
         {
+            string menu = String.Format($"1. Build a car\n2. Remove a car\n" +
+                $"3. List all cars\n4. Exit\n\nEnter your option: ");
+
+            Car myCar = null;
+            Car[] cars = new Car[0];
+
+            while(true)
+            {
+                Console.Write(menu);
+
+                switch(int.Parse(Console.ReadLine()))
+                {
+                    case 1: //Build a car
+                        Console.Write("Enter the year: ");
+                        int year = int.Parse(Console.ReadLine());
+                        Console.Write("Enter the color of the car: ");
+                        string color = Console.ReadLine();
+                        Console.Write("Enter the car type: ");
+                        string carType = Console.ReadLine();
+                        Console.Write("Enter the make of the car: ");
+                        string make = Console.ReadLine();
+                        Console.Write("Enter the model of the car: ");
+                        string model = Console.ReadLine();
+                        Console.Write("Enter the gas tank capacity: ");
+                        int gtc = int.Parse(Console.ReadLine());
+                        myCar = new Car(year, color, gtc, make, model, carType);
+                        Array.Resize(ref cars, cars.Length + 1);
+                        cars[cars.Length - 1] = myCar;
+                        break;
+
+                    case 2: //Remove a car
+                        Console.Write($"Enter the number to remove a car (1 - {cars.Length}):");
+                        int choice = int.Parse(Console.ReadLine());
+                        //choice = choice - 1;
+                        //Array.Clear(cars, --choice, 1);
+                        for (int j = choice - 1; j < cars.Length - 1; j++)
+                        {
+                            cars[j] = cars[j + 1];
+                        }
+                        //Shift all car objects over by one based on choice
+                        for(int k = choice; k < cars.Length; k++)
+                        {
+                            cars[choice - 1] = cars[choice];
+                        }
+                        //cars[choice] = null;
+                        Array.Resize(ref cars, cars.Length - 1);
+                        break;
+
+                    case 3: //List all cars
+                        int i = 1;
+                        foreach(Car c in cars)
+                        {
+                            Console.WriteLine($"============Car #{i}==============");
+                            Console.WriteLine(c);
+                            Console.WriteLine("");
+                            i++;
+                        }
+                        Console.WriteLine("\n");
+                        break;
+
+                    case 4: //Exit the program - so easy a cave man can do this!  Sponsored by GEICO
+                        Environment.Exit(0);
+                        break;
+
+                    default: Console.WriteLine("Invalid option");
+                        continue;
+                }
+            }
+
             //I want to create a car object
-            Car myCar = new Car();
+           /* Car myCar = new Car();
             
             myCar.Color = "Red";
             myCar.CarType = "SUV";
@@ -72,7 +141,7 @@ namespace CarFactory
                 Color = "Red",
                 GasTankCapacity = 14.5,
                 IsRunning = true
-            };
+            };*/
         }
     }
 }
