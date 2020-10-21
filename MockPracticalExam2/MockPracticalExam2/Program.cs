d4fcb38f036c4a3017b3e25d29a59b52889f0f87
Brian Trager <bptnbs@rit.edu>
2020-10-21 08:10:44 -0400

    Initial commit

---
+++ MockPracticalExam2/MockPracticalExam2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MockPracticalExam2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice;
+
+            DisplayHeading();
+
+            while (true)
+            {
+                choice = GetMainMenuChoice();
+
+                switch (choice)
+                {
+                    case 1:
+                        EnterASighting();
+                        break;
+                    case 2:
+                        DisplayAllSightings();
+                        break;
+                    case 3:
+                        FilterBySpecies();
+                        break;
+                    case 4:
+                        DisplayRecordCount();
+                        break;
+                    case 5:
+                        ExitProgram();
+                        break;
+                }
+            }
+        }
+    }
+}
