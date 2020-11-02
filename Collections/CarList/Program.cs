0fff85c57039f86e9a076a71c03fd7f1aee31c80
Brian Trager <bptnbs@rit.edu>
2020-11-02 08:53:51 -0500

    Collections

---
+++ Collections/CarList/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CarList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> carList = new List<Car>();
+
+            carList.Add(new Car() { Year = 2020, Make = "Honda", Model = "Civic" });
+            carList.Add(new Car() { Year = 2016, Make = "Ford", Model = "Mustang" });
+
+
+            foreach(Car c in carList)
+            {
+                Console.WriteLine(c);
+            }
+        }
+    }
+
+    class Car
+    {
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format($"Year: {this.Year}\nMake: {this.Make}\nModel: {this.Model}\n");
+        }
+    }
+}
---
+++ Collections/Collections/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+
+            //numbers.Add(5);
+            //numbers.Add(8);
+            int n;
+            while(true)
+            {
+                Console.Write("Enter a number: ");
+                if ((n = int.Parse(Console.ReadLine())) != -99)
+                    numbers.Add(n);
+                else
+                    break;
+            }
+
+            //int[] numbers2 = new int[50000];
+
+            Console.WriteLine($"There are {numbers.Count} elements in the list");
+
+            foreach(int num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
