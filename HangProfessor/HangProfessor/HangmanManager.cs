e0c0a6fa9e933b6c903101871049c090bcda6e63
Brian Trager <bptnbs@rit.edu>
2020-11-11 09:51:34 -0500

    init commit

---
+++ HangProfessor/HangProfessor/HangmanManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HangProfessor
+{
+    class HangmanManager
+    {
+        //store the hidden word
+        private string hiddenString;
+        //A public read only auto property to store the string of correctly guessed characters and asterisks.
+        public string DisplayString { get; private set; }
+
+        //A public read only property to indicate if the game is over.
+        public bool GameOver {
+            get
+            {
+                return DisplayString == hiddenString;
+            }
+        }
+
+        //A constructor that accepts the hidden word to display.
+        public HangmanManager(string word)
+        {
+            hiddenString = word;
+
+            CreateDisplayString();
+        }
+
+        public void CreateDisplayString()
+        {
+            for (int i = 0; i < hiddenString.Length; i++)
+                DisplayString += "*";
+        }
+
+        public bool Guess(char c)
+        {
+            bool flag = false;
+            for(int i = 0; i < hiddenString.Length; i++)
+            {
+                if(c == hiddenString[i])
+                {
+                    DisplayString = DisplayString.Remove(i, 1);
+                    DisplayString = DisplayString.Insert(i, c.ToString());
+                    flag = true;
+                }
+            }
+
+            return flag;
+        }
+    }
+}
---
+++ HangProfessor/HangProfessor/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HangProfessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = { "fall", "winter", "spring", "summer", "tiger", "semester" };
+            Random rng = new Random();
+
+
+            //Create a Hangman variable
+            HangmanManager hangman;
+
+            //Instantiate your HangmanManager variable.
+            hangman = new HangmanManager(words[rng.Next(words.Length)]);
+
+            do
+            {
+                Console.WriteLine(hangman.DisplayString);
+                Console.Write("Enter a single character: ");
+                char guess = char.Parse(Console.ReadLine());
+                if(hangman.Guess(guess))
+                    Console.WriteLine("Correct!");
+                else
+                    Console.WriteLine($"Cannot find the letter {guess}");
+                //hangman.Guess(char.Parse(Console.ReadLine());
+
+            }while(!hangman.GameOver);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Winner!");
+        }
+    }
+}
