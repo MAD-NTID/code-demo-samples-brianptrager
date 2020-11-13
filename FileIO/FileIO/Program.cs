5c069d84a106c32b3fd39de923e78a5c9d7ff385
Brian Trager <bptnbs@rit.edu>
2020-11-13 09:19:07 -0500

    init commit

---
+++ FileIO/FileIO/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace FileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = null;
+            using (StreamReader reader = new StreamReader("pf1.txt"))
+            {
+                while ((str = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(str);
+                }
+            }
+        }
+    }
+}
